267152e5faa6f6688e397c99cbf746e40eeb31d1
kbt <42253042+littledigits@users.noreply.github.com>
2018-08-11 20:39:36 +0200

    index.php
    Uproszczenie widoku przez usunięcie odnośników do wersji +php i +php+sql

--- index.php
+++ index.php
@@ -27,16 +27,13 @@ function indexinfo($folder, $opis, $disabled) {
 }
 //indexinfo("bootstrap", "W przyszłości");
 indexinfo ("html", "Czysty HTML z dodatkiem Font awesome", '');
-indexinfo ("html+php", "Treść jest odczytywana z wersji html i zamieniana w celu wyświetlenia w motywie norton commander", '');
-indexinfo ("html+php+sql", 'Celem były eksperymenty z &ltiframe&gt. Wariant porzucony', 'disabled');
 indexinfo ("html+php+sql+js", "Aktualnie najbardziej zaawansowany wariant", '');
 ?>
 </nav>
 <footer>
-<p>theme: DOS_like</p>
-<p class="mid-big">HTML5 <i class="fab fa-html5"></i>, font awesome <i class="fab fa-font-awesome"></i> Podziękowania dla: Borsoft, P. Paprzycki</p>
-<p class="mid-big">alfabet Morse'a &#8226;&#8211;&#8226;&#8226;|&#8226;&#8226;|&#8211;|&#8211;|&#8226;&#8211;&#8226;&#8226;|&#8226;|&#8211;&#8226;&#8226;|&#8226;&#8226;|&#8211;&#8211;&#8226;|&#8226;&#8226;|&#8211;|&#8226;&#8226;&#8226;</p>
-<p class="mid-big">kod binarny <code>01101100 01101001 01110100 01110100 01101100 01100101 01100100 01101001 01100111 01101001 01110100 01110011</code></p>
+<?php
+include $url.'cv_html+php+sql+js/footer.txt';
+?>
 </footer>
 </body>
 </html>
