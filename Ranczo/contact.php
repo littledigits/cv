f3c17e15ccd0bd308e7f6fb508612b9682170f06
kbt <42253042+littledigits@users.noreply.github.com>
2018-10-28 09:27:50 +0100

    added Ranczo project

---
+++ Ranczo/contact.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html>
+	<head>
+<title>Kontakt - Ranczo pod pomarańczą</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link href="bootstrap-4.1.3-dist/css/bootstrap.css" rel="stylesheet">
+		<link href="fontawesome-free-5.2.0-web/css/all.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Smokum|Caveat" rel="stylesheet">
+		<script src="jquery-3.3.1.js"></script>
+	</head>
+	<body>
+		<div id="main-container" class="container-fluid">
+			<header>
+				<?php
+					include 'header.html';
+				?>
+			</header> 
+			<nav>
+				<div class="row justify-content-center d-none d-lg-flex pb-0">
+					<ul class="nav nav-tabs border-0">
+						<li class="nav-item"><a href="index.php" class="nav-link px-5 mx-2 bg-warning text-secondary">O nas</a></li>
+						<li class="nav-item"><a href="offer.php" class="nav-link px-5 mx-2 bg-warning text-secondary">Oferta</a></li>
+						<li class="nav-item"><a href="attractions.php" class="nav-link px-5 mx-2 bg-warning text-secondary">Atrakcje</a></li>
+						<li class="nav-item"><a href="gallery.php" class="nav-link px-5 mx-2 bg-warning text-secondary">Galeria</a></li>
+						<li class="nav-item"><a href="#" class="nav-link px-5 mx-2 bg-light text-dark">Kontakt</a></li>
+					</ul>
+				</div>
+
+				<div class="row bg-transparent navbar navbar-light d-lg-none">
+					<a class="navbar-brand text-warning" href="#">Ranczo pod pomarańczą</a>
+					<button class="navbar-toggler mb-1 bg-warning" type="button" data-toggle="collapse" data-target="#small-menu" aria-controls="small-menu" aria-expanded="false" aria-label="Toggle navigation">
+						<i class="navbar-toggler-icon"></i>
+					</button>
+					<div class="collapse navbar-collapse" id="small-menu">
+						<ul class="navbar-nav nav-pills">
+							<li class="nav-item"><a href="index.php" class="nav-link px-5 mx-3 bg-warning text-secondary">O nas</a></li>
+							<li class="nav-item"><a href="offer.php" class="nav-link px-5 mx-3 bg-warning text-secondary">Oferta</a></li>
+							<li class="nav-item"><a href="attractions.php" class="nav-link px-5 mx-3 bg-warning text-secondary">Atrakcje</a></li>
+							<li class="nav-item"><a href="gallery.php" class="nav-link px-5 mx-3 bg-warning text-secondary">Galeria</a></li>
+							<li class="nav-item"><a href="#" class="nav-link px-5 mx-3 bg-light text-dark">Kontakt</a></li>
+							<li class="nav-item dropdown mt-1">
+								<a href="#" class="nav-link px-5 mx-3 bg-warning text-secondary dropdown-toggle" id="language-dropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="flase">Lang&nbsp;
+									<i class="fas fa-globe"></i>
+								</a>
+								<ul class="dropdown-menu bg-transparent border-0 m-0 p-0" aria-labelledby="navbarDropdown">
+									<li><a href="#" class="nav-link px-5 mx-3 bg-info text-light"><samp>EN&nbsp;</samp><img src="img/en.gif"></a></li>
+									<li><a href="#" class="nav-link px-5 mx-3 bg-info text-light"><samp>FR&nbsp;</samp><img src="img/fr.gif"></a></li>
+									<li><a href="#" class="nav-link px-5 mx-3 bg-info text-light"><samp>DE&nbsp;</samp><img src="img/de.gif"></a></li>
+									<li><a href="#" class="nav-link px-5 mx-3 bg-info text-light"><samp>IT&nbsp;</samp><img src="img/it.gif"></a></li>
+									<li><a href="#" class="nav-link px-5 mx-3 bg-info text-light"><samp>RU&nbsp;</samp><img src="img/ru.gif"></a></li>
+									<li><a href="#" class="nav-link px-5 mx-3 bg-info text-light"><samp>ES&nbsp;</samp><img src="img/es.gif"></a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+
+			<div class="row">
+				<div class="col-sm-1">
+					<ul class="nav d-none d-lg-block">
+								<li><a href="#" class="btn btn-info p-1"><samp>EN&nbsp;</samp><img src="img/en.gif"></a></li>
+								<li><a href="#" class="btn btn-info p-1"><samp>FR&nbsp;</samp><img src="img/fr.gif"></a></li>
+								<li><a href="#" class="btn btn-info p-1"><samp>DE&nbsp;</samp><img src="img/de.gif"></a></li>
+								<li><a href="#" class="btn btn-info p-1"><samp>IT&nbsp;</samp><img src="img/it.gif"></a></li>
+								<li><a href="#" class="btn btn-info p-1"><samp>RU&nbsp;</samp><img src="img/ru.gif"></a></li>
+								<li><a href="#" class="btn btn-info p-1"><samp>ES&nbsp;</samp><img src="img/es.gif"></a></li>
+							</ul>
+						</div>
+				<div class="col-xs-12 col-sm-10 bg-light jumbotron">
+					<section><h3 class="text-secondary">KONTAKT</h3></section>
+					
+					<article class="text-dark">
+						<a class="btn btn-outline-dark rounded p-2 mb-1" href="mailto:info@ranczopp.com"><i class="fas fa-envelope"></i>&nbsp;info@ranczopp.com</a>
+						<a class="btn btn-outline-dark rounded p-2 mb-1" href="tel:+48-793-77-37-99"><i class="fas fa-phone-square"></i>&nbsp;PL: (+48) 793 77 37 99</a>
+						<a class="btn btn-outline-dark rounded p-2 mb-1" href="tel:+49-162-677-78-88"><i class="fas fa-phone-square"></i>&nbsp;DE: (+49) 162 677 78 88</a>
+						<a class="btn btn-outline-dark rounded p-2 mb-1" href="#"><i class="fas fa-map-marked"></i>&nbsp;Polany 5 A, 33-380 Krynica Zdrój</a>
+						<a class="btn btn-outline-dark rounded p-2 mb-1" href="#">NR. RACHUNKU BANKOWEGO: <br class="d-xs-block d-md-none">57 1470 0002 2106 2786 7141 0001</a>
+						<div class="border-1 px-1 pt-1 bg-warning mx-auto">
+							<iframe class="mx-auto w-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2589.9796980500223!2d20.98831821569799!3d49.52265897935802!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473ddc7318c3d9e3%3A0x4cb4d9eb336ac0cf!2sPolany+5A%2C+33-380+Polany!5e0!3m2!1spl!2spl!4v1537984550565" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+						</div>
+					</article>
+				
+				</div>
+				<div class="col-sm-1"></div>				
+			</div>
+
+			<footer>
+				<div class="row justify-content-center">
+					<a href="http://littledigits.pl" target="_blank" class="btn btn-danger py-0"><small>Creatively developed by littleDIGITS</small></a>
+				</div>
+			</footer>
+		</div>
+
+		<script src="bootstrap-4.1.3-dist/js/bootstrap.js"></script>
+
+	</body>
+</html>
---
+++ Ranczo/offer.php
@@ -0,0 +1,162 @@
+<!doctype html>
+<html>
+	<head>
+<title>Oferta - Ranczo pod pomarańczą</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link href="bootstrap-4.1.3-dist/css/bootstrap.css" rel="stylesheet">
+		<link href="fontawesome-free-5.2.0-web/css/all.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Caveat" rel="stylesheet">
+		<script src="jquery-3.3.1.js"></script>
+	</head>
+	<body>
+		<div id="main-container" class="container-fluid">
+			<header>
+				<?php
+					include 'header.html';
+				?>
+			</header> 
+			<nav>
+				<div class="row justify-content-center d-none d-lg-flex pb-0">
+					<ul class="nav nav-tabs border-0">
+						<li class="nav-item"><a href="index.php" class="nav-link px-5 mx-2 bg-warning text-secondary">O nas</a></li>
+						<li class="nav-item"><a href="#" class="nav-link px-5 mx-2 bg-light text-dark">Oferta</a></li>
+						<li class="nav-item"><a href="attractions.php" class="nav-link px-5 mx-2 bg-warning text-secondary">Atrakcje</a></li>
+						<li class="nav-item"><a href="gallery.php" class="nav-link px-5 mx-2 bg-warning text-secondary">Galeria</a></li>
+						<li class="nav-item"><a href="contact.php" class="nav-link px-5 mx-2 bg-warning text-secondary">Kontakt</a></li>
+					</ul>
+				</div>
+
+				<div class="row bg-transparent navbar navbar-light d-lg-none">
+					<a class="navbar-brand text-warning" href="#">Ranczo pod pomarańczą</a>
+					<button class="navbar-toggler mb-1 bg-warning" type="button" data-toggle="collapse" data-target="#small-menu" aria-controls="small-menu" aria-expanded="false" aria-label="Toggle navigation">
+						<i class="navbar-toggler-icon"></i>
+					</button>
+					<div class="collapse navbar-collapse" id="small-menu">
+						<ul class="navbar-nav nav-pills">
+							<li class="nav-item"><a href="index.php" class="nav-link px-5 mx-3 bg-warning text-secondary">O nas</a></li>
+							<li class="nav-item"><a href="#" class="nav-link px-5 mx-3 bg-light text-dark">Oferta</a></li>
+							<li class="nav-item"><a href="attractions.php" class="nav-link px-5 mx-3 bg-warning text-secondary">Atrakcje</a></li>
+							<li class="nav-item"><a href="gallery.php" class="nav-link px-5 mx-3 bg-warning text-secondary">Galeria</a></li>
+							<li class="nav-item"><a href="contact.php" class="nav-link px-5 mx-3 bg-warning text-secondary">Kontakt</a></li>
+							<li class="nav-item dropdown mt-1">
+								<a href="#" class="nav-link px-5 mx-3 bg-warning text-secondary dropdown-toggle" id="language-dropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="flase">Lang&nbsp;
+									<i class="fas fa-globe"></i></i>
+								</a>
+								<ul class="dropdown-menu bg-transparent border-0 m-0 p-0" aria-labelledby="navbarDropdown">
+									<li><a href="#" class="nav-link px-5 mx-3 bg-info text-light"><samp>EN&nbsp;</samp><img src="img/en.gif"></a></li>
+									<li><a href="#" class="nav-link px-5 mx-3 bg-info text-light"><samp>FR&nbsp;</samp><img src="img/fr.gif"></a></li>
+									<li><a href="#" class="nav-link px-5 mx-3 bg-info text-light"><samp>DE&nbsp;</samp><img src="img/de.gif"></a></li>
+									<li><a href="#" class="nav-link px-5 mx-3 bg-info text-light"><samp>IT&nbsp;</samp><img src="img/it.gif"></a></li>
+									<li><a href="#" class="nav-link px-5 mx-3 bg-info text-light"><samp>RU&nbsp;</samp><img src="img/ru.gif"></a></li>
+									<li><a href="#" class="nav-link px-5 mx-3 bg-info text-light"><samp>ES&nbsp;</samp><img src="img/es.gif"></a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+
+			<div class="row">
+				<div class="col-sm-1">
+					<ul class="nav d-none d-lg-block">
+								<li><a href="#" class="btn btn-info p-1"><samp>EN&nbsp;</samp><img src="img/en.gif"></a></li>
+								<li><a href="#" class="btn btn-info p-1"><samp>FR&nbsp;</samp><img src="img/fr.gif"></a></li>
+								<li><a href="#" class="btn btn-info p-1"><samp>DE&nbsp;</samp><img src="img/de.gif"></a></li>
+								<li><a href="#" class="btn btn-info p-1"><samp>IT&nbsp;</samp><img src="img/it.gif"></a></li>
+								<li><a href="#" class="btn btn-info p-1"><samp>RU&nbsp;</samp><img src="img/ru.gif"></a></li>
+								<li><a href="#" class="btn btn-info p-1"><samp>ES&nbsp;</samp><img src="img/es.gif"></a></li>
+							</ul>
+				</div>
+				<div class="col-xs-12 col-sm-10 bg-light jumbotron">
+					<h3 class="text-secondary">OFERTA</h3>
+					<p>Nasze Ranczo czeka na Państwa w sercu Beskidu Sądeckiego w małej miejscowości Polany. Malownicze położenie w otoczeniu łąk i lasów pozwoli Państwu na wypoczynek zarówno duchowy, jak i fizyczny.</p>
+					<p>Wspaniała okolica obfituje w miejsca i enklawy do zbierania grzybów - borowiki, rydze, maślaki, to tylko przedsmak tego co można znaleźć.
+					Fani wędkarstwa rownież znajdą coś dla siebie <i class="fas fa-grin"></i></p>
+					<hr>
+					<p>Nasza oferta dotyczy wynajmu domu o powierzchni mieszkalnej 135 m<sup>2</sup>.</p>
+					<p>Oddajemy do Państwa dyspozycji:</p>
+					<p class="lead">PARTER</p>
+					<ul class="list-unstyled ml-sm-3">
+						<li><i class="fas fa-asterisk"></i>&nbsp;obszerny korytarz</li>
+						<li><i class="fas fa-asterisk"></i>&nbsp;jadalnia (duży stół + 10 krzeseł, rozkładana sofa)</li>
+						<li><i class="fas fa-asterisk"></i>&nbsp;kuchnia w pełnym wyposażeniu (płyta indukcyjna, lodówka, zmywarka, piekarnik, mikrofalówka, expres, czajnik, garnki, talerze, szklanki, sztućce itp.)</li>
+						<li><i class="fas fa-asterisk"></i>&nbsp;łazienka (prysznic, umywalka, wc, ręczniki)</li>
+					</ul>
+
+					<p class="lead">PIĘTRO - 3 sypialnie w tym:</p>
+					<ul class="list-unstyled ml-sm-3">
+						<li><i class="fas fa-asterisk"></i>&nbsp;pokój 4 osobowy (1 łóżko podwójne + sofa)</li>
+						<li><i class="fas fa-asterisk"></i>&nbsp;pokój 3 osobowy (1 łóżko podwójne + 1 pojedyncze)</li>
+						<li><i class="fas fa-asterisk"></i>&nbsp;pokój 2 osobowy (1 łóżko podwójne)</li>
+						<li><i class="fas fa-asterisk"></i>&nbsp;Salon z telewizorem (rozkładany narożnik)</li>
+						<li><i class="fas fa-asterisk"></i>&nbsp;łazienka (wanna, umywalka, wc, ręczniki)</li>
+						<li><i class="fas fa-asterisk"></i>&nbsp;taras</li>
+					</ul>
+					<p class="lead">UDOGODNIENIA</p>
+					<ul class="list-unstyled ml-sm-3">
+						<li><i class="fas fa-asterisk"></i>&nbsp;duży ogród</li>
+						<li><i class="fas fa-asterisk"></i>&nbsp;zadaszony parking</li>
+						<li><i class="fas fa-asterisk"></i>&nbsp;plac zabaw</li>
+						<li><i class="fas fa-asterisk"></i>&nbsp;trampolina</li>
+						<li><i class="fas fa-asterisk"></i>&nbsp;duży rodzinny basen (latem)</li>
+						<li><i class="fas fa-asterisk"></i>&nbsp;grill</li>
+						<li><i class="fas fa-asterisk"></i>&nbsp;telewizja satelitarna</li>
+						<li><i class="fas fa-asterisk"></i>&nbsp;wi-fi</li>
+					</ul>
+
+					<p>Gwarantujemy Państwu wysoki standard obiektu w pełnym wyposażeniu RTV i AGD.</p>
+					<p class="lead">CENNIK WYNAJMU DOMU</p>
+					
+					<table class="table table-striped table-hover">
+						<caption>* dom komfortowo ugości do 12 osób</caption>
+						<tr class="bg-warning">
+							<th scope="col">terminy</th>
+							<th scope="col">cena wynajmu domu [PLN / dobę]*</th>
+						</tr>
+						<tr>
+							<td>05 styczeń - 31 marzec</td>
+							<td>550,00</td>
+						</tr>
+						<tr>
+							<td>01 kwiecień - 30 kwiecień</td>
+							<td>430,00</td>
+						</tr>
+						<tr>
+							<td>01 maj - 07 maj</td>
+							<td>550,00</td>
+						</tr>
+						<tr>
+							<td>08 maj - 30 wrzesień</td>
+							<td>500,00</td>
+						</tr>
+						<tr>
+							<td>01 październik - 30 listopad</td>
+							<td>430,00</td>
+						</tr>
+						<tr>
+							<td>01 grudzień - 29 grudzień</td>
+							<td>550,00</td>
+						</tr>
+						<tr>
+							<td>30 grudzień - 04 styczeń</td>
+							<td>800,00</td>
+						</tr>
+					</table>
+
+				</div>
+				<div class="col-sm-1"></div>				
+			</div>
+
+			<footer>
+				<div class="row justify-content-center">
+					<a href="http://littledigits.pl" target="_blank" class="btn btn-danger py-0"><small>Creatively developed by littleDIGITS</small></a>
+				</div>
+			</footer>
+		</div>
+
+		<script src="bootstrap-4.1.3-dist/js/bootstrap.js"></script>
+
+	</body>
+</html>
