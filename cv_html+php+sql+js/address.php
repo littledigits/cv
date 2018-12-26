68a6a90ac61907ae18448074314c7a1851f4b3e6
kbt <42253042+littledigits@users.noreply.github.com>
2018-12-26 18:52:47 +0100

    Adress
    Content is divided into two sections and floating on big screens

--- cv_html+php+sql+js/address.php
+++ cv_html+php+sql+js/address.php
@@ -44,6 +44,7 @@ if (isSet($_cookie['css'])) {
 			<?php include "nav.html"; ?>
 		</div>
 <div id="iframe-right">
+<section>
 <h2 style="display: none;">Dane podstawowe</h2>
 <!--a href="https://www.google.pl/maps/@52.1283143,21.0604289,3a,75y,263.94h,89.61t/data=!3m6!1e1!3m4!1slO2cxsw3NaATqgpVCCrIcA!2e0!7i13312!8i6656!6m1!1e1" target="_blank" title="Google Street View"><i class="fa fa-street-view"></i></a>
 <a href="https://www.bing.com/maps?osid=085a8d97-4481-485b-9c3a-adbb807dabd0&cp=52.128366~21.059288&lvl=18&v=2&sV=2&form=S00027" target="_blank">(dojazd BING)</a> <a href="https://wego.here.com/directions/mix//ulica-Romualda-Mielczarskiego-1,-02-798-Warszawa,-Polska:loc-dmVyc2lvbj0xO3RpdGxlPXVsaWNhK1JvbXVhbGRhK01pZWxjemFyc2tpZWdvKzE7bGFuZz1wbDtsYXQ9NTIuMTI4Mjk5NzEzMTM0NzY2O2xvbj0yMS4wNjAxMjkxNjU2NDk0MTQ7c3RyZWV0PXVsaWNhK1JvbXVhbGRhK01pZWxjemFyc2tpZWdvO2hvdXNlPTE7Y2l0eT1XYXJzemF3YTtwb3N0YWxDb2RlPTAyLTc5ODtjb3VudHJ5PVBPTDtkaXN0cmljdD1VcnN5biVDMyVCM3c7c3RhdGU9V29qLitNYXpvd2llY2tpZTtjb3VudHk9V2Fyc3phd2E7Y2F0ZWdvcnlJZD1idWlsZGluZztzb3VyY2VTeXN0ZW09aW50ZXJuYWw7bmxhdD01Mi4xMjgzMTExNTcyMjY1NjtubG9uPTIxLjA2MDQ0OTYwMDIxOTcyNw?map=52.1283,21.06013,15,normal" target="_blank">(HERE MAPS)</a> <a href="http://jakdojade.pl/?tc=52.1283:21.060129&tn=Romualda%20Mielczarskiego%201&td=Romualda%20Mielczarskiego%201&cid=3000&aro=1" target="_blank">(dojazd jakdojade.pl)</a-->
@@ -84,16 +85,21 @@ mysql_close($connection);
 ?>
 
 <br>
+</section>
+<section>
 <form action="">
-<h2 style="display: none;">Wyszukiwanie tagów</h2><input type="text" name="tekst" id="pole_selekcji" autofocus autocomplete="off" onkeyup="selekcja(this.value)">
+<h2 style="display: none;">Wyszukiwanie tagów</h2>
+<input type="text" name="tekst" id="pole_selekcji" autofocus autocomplete="off" onkeyup="selekcja(this.value)">
 <p id="wynik_selekcji"></p>
 </form>
 
+<h2 style="display: none;">little DIGITAL clock</h2>
 <?php
 include $urladdress.'widgets/clock.html';
 ?>
 
 </div>
+</section>
 </article>
 
 <footer id="footer-dimension">
