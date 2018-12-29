ce288e7879b8ef028e65f8bdbff6b19e03435d7e
kbt <42253042+littledigits@users.noreply.github.com>
2018-12-29 19:08:23 +0100

    Update index.php
    1. Added screen resolution reader to print objects correctly on small devices
    2. Changed some objects to better layout on small devices

--- index.php
+++ index.php
@@ -3,17 +3,15 @@ include 'meta.php';
 ?>
 <!doctype html>
 <html lang="pl">
-<title>intro 0.4</title>
+<title>intro 0.6 - littleDIGITS</title>
 <meta charset="utf-8">
 <link rel="shortcut icon" href="images/logo.png">
 <style>
 	* {font-family: Lato, Share Tech Mono, Lucida Console;cursor: default;}
-	p {font-size: 150%;text-align: center;display: none;color: white;}
-	.p-loading {display: block;margin: 0;padding: 0;}
-	.loading-bar {display: block;}
-	.loading-text {text-align: left;}
-	div.loading {font-size: 150%;text-align: center;color: white;float: left;}
-	.span-loading {color: black;background: white;width: 0;position: absolute;margin-left: 1%;}
+	p {font-size: 24px;text-align: center;display: none;color: white;}
+	.p-loading {display: block;margin: 0;padding: 0;vertical-align: middle;}
+	.loading {font-size: 24px;color: white;width: 100%;}
+	.span-loading {color: black;background: white;width: 0;margin: 0;padding: 0;vertical-align: middle;text-align: center;height: 24px;}
 	#href-menu, #href-reload {color:white;text-shadow: black 0 0 1px;text-decoration: none;}
 	#href-menu:hover, #href-reload:hover, .fa-redo:hover {font-size: 150%;cursor: pointer;}
 	a {cursor: pointer;}
@@ -38,20 +36,19 @@ include 'meta.php';
 	<p>Mam nadzieję, że przyciągnąłem Twoją uwagę i możemy przejść dalej?</p>
 	<p><span id="span-tak" style="width: 50%;float: left;text-decoration: underline;cursor: pointer;">tak</span><span id="span-nie" style="width: 50%;float: right;text-decoration: underline;">nie</span></p>
 	<div id="div-2" style="background: black;height: 100%;width: 100%;position: fixed;top: 0;left: 0;display: none;overflow: auto;">
-		<div class="loading" style="width: 40%;text-align: right;">
-			<div id="p-loading loading-text" class="p-loading"><span>kreatywność - ładowanie</span></div><br>
-			<div id="p-loading loading-text" class="p-loading"><span>dokładność</span><span> - ładowanie </span></div><br>
-			<div id="p-loading loading-text" class="p-loading"><span>zaangażowanie</span><span> - ładowanie </span></div><br>
-			<div id="p-loading loading-text" class="p-loading"><span>samodzielność</span><span> - ładowanie </span></div><br>
-			<div id="p-loading loading-text" class="p-loading"><span>wiedza</span><span> - ładowanie </span></div>
-		</div>
+
 		<div class="loading">
-			<div class="p-loading loading-bar"><span id="creativity-loading" class="span-loading"><span id="creativity-loading-caption" style="visibility: hidden;">100%</span></span></div><br><br>
-			<div class="p-loading loading-bar"><span id="precision-loading" class="span-loading"><span id="precision-loading-caption" style="visibility: hidden;">100%</span></span></div><br><br>
-			<div class="p-loading loading-bar"><span id="engagement-loading" class="span-loading"><span id="engagement-loading-caption" style="visibility: hidden;">100%</span></span></div><br><br>
-			<div class="p-loading loading-bar"><span id="self-reliance-loading" class="span-loading"><span id="self-reliance-loading-caption" style="visibility: hidden;">100%</span></span></div><br><br>
-			<div class="p-loading loading-bar"><span id="knowledge-loading" class="span-loading"><span id="knowledge-loading-caption" style="visibility: hidden;">100%</span></span></div>
+			<div class="p-loading"><span>kreatywność - ładowanie </span><br><span id="creativity-loading" class="span-loading"><span id="creativity-loading-caption" style="visibility: hidden;"></span></span></div>
+			
+			<div class="p-loading"><span>dokładność - ładowanie </span><br><span id="precision-loading" class="span-loading"><span id="precision-loading-caption" style="visibility: hidden;"></span></span></div>
+			
+			<div class="p-loading"><span>zaangażowanie - ładowanie </span><br><span id="engagement-loading" class="span-loading"><span id="engagement-loading-caption" style="visibility: hidden;"></span></span></div>
+			
+			<div class="p-loading"><span>samodzielność - ładowanie </span><br><span id="self-reliance-loading" class="span-loading"><span id="self-reliance-loading-caption" style="visibility: hidden;"></span></span></div>
+			
+			<div class="p-loading"><span>wiedza - ładowanie </span><br><span id="knowledge-loading" class="span-loading"><span id="knowledge-loading-caption" style="visibility: hidden;"></span></span></div>
 		</div>
+		
 		<br style="clear: left;">
 		<br>
 		<div id="motivation-letter" style="width: 100%;display: none;margin-top: 5%;">
@@ -64,10 +61,10 @@ include 'meta.php';
 <script>
 	$(document).ready(function(){
 		var i = 1500;
-		$('#div-1').fadeTo(2500, 0, 'linear', function(){
+		$('#div-1').fadeTo(i*1.6666666666666666666666666666667, 0, 'linear', function(){
 			$('#div-1').remove();
 		});
-		$('p').delay(2500).eq(0).fadeIn(i, function(){
+		$('p').delay(i*1.6666666666666666666666666666667).eq(0).fadeIn(i, function(){
 			$('p').eq(1).fadeIn(i*0.8, function(){
 				$('p').eq(2).fadeIn(i*0.7, function(){
 					$('p').eq(3).fadeIn(i*0.6, function(){
@@ -92,21 +89,28 @@ include 'meta.php';
 		});
 	});
 	$('#span-tak').click(function(){
+		var screenWidth = document.body.clientWidth;
+		if (screenWidth < 480) {spanWidth = '100%'; brVisible = ''} else {spanWidth = '30%'; brVisible = 'br';}
+		$(brVisible).css({display: 'none'});
 		$('#div-2').eq(0).fadeIn(1000).css({display: 'inlineBlock'});
-		$('#creativity-loading').delay(1000).animate({width: '30%'}, 1000, function(){
+		$('#creativity-loading').delay(1000).animate({width: spanWidth}, 1000, function(){
 			$('#creativity-loading-caption').css({visibility: 'visible'});
+			$('#creativity-loading-caption').text('100%');
 		});
-		$('#precision-loading').delay(2200).animate({width: '30%'}, 1200, function(){
+		$('#precision-loading').delay(2200).animate({width: spanWidth}, 1200, function(){
 			$('#precision-loading-caption').css({visibility: 'visible'});
+			$('#precision-loading-caption').text('100%');
 		});
-		$('#engagement-loading').delay(3400).animate({width: '30%'}, 1400, function(){
+		$('#engagement-loading').delay(3400).animate({width: spanWidth}, 1400, function(){
 			$('#engagement-loading-caption').css({visibility: 'visible'});
+			$('#engagement-loading-caption').text('100%');
 		});
-		$('#self-reliance-loading').delay(4800).animate({width: '30%'}, 1400, function(){
+		$('#self-reliance-loading').delay(4800).animate({width: spanWidth}, 1400, function(){
 			$('#self-reliance-loading-caption').css({visibility: 'visible'});
+			$('#self-reliance-loading-caption').text('100%');
 		});
 		var x = $('#knowledge-loading');
-		x.delay(6200).animate({width: '30%'}, 4000, function(){
+		x.delay(6200).animate({width: spanWidth}, 4000, function(){
 			$('#knowledge-loading-caption').css({visibility: 'visible'});
 			x.css({backgroundColor: 'red'});
 			$('#knowledge-loading-caption').text('wciąż ładuję');
