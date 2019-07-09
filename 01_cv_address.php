16cc94b2de05eb0c25c035b1af8acf79f2dbb734
kbt <kbt@littledigits.p>
2019-07-09 22:49:33 +0200

    personal data like JSON

--- 01_cv_address.php
+++ 01_cv_address.php
@@ -33,6 +33,8 @@
 						<h2 style="display: none;">Dane podstawowe</h2>
 					<!--a href="https://www.google.pl/maps/@52.1283143,21.0604289,3a,75y,263.94h,89.61t/data=!3m6!1e1!3m4!1slO2cxsw3NaATqgpVCCrIcA!2e0!7i13312!8i6656!6m1!1e1" target="_blank" title="Google Street View"><i class="fa fa-street-view"></i></a>
 					<a href="https://www.bing.com/maps?osid=085a8d97-4481-485b-9c3a-adbb807dabd0&cp=52.128366~21.059288&lvl=18&v=2&sV=2&form=S00027" target="_blank">(dojazd BING)</a> <a href="https://wego.here.com/directions/mix//ulica-Romualda-Mielczarskiego-1,-02-798-Warszawa,-Polska:loc-dmVyc2lvbj0xO3RpdGxlPXVsaWNhK1JvbXVhbGRhK01pZWxjemFyc2tpZWdvKzE7bGFuZz1wbDtsYXQ9NTIuMTI4Mjk5NzEzMTM0NzY2O2xvbj0yMS4wNjAxMjkxNjU2NDk0MTQ7c3RyZWV0PXVsaWNhK1JvbXVhbGRhK01pZWxjemFyc2tpZWdvO2hvdXNlPTE7Y2l0eT1XYXJzemF3YTtwb3N0YWxDb2RlPTAyLTc5ODtjb3VudHJ5PVBPTDtkaXN0cmljdD1VcnN5biVDMyVCM3c7c3RhdGU9V29qLitNYXpvd2llY2tpZTtjb3VudHk9V2Fyc3phd2E7Y2F0ZWdvcnlJZD1idWlsZGluZztzb3VyY2VTeXN0ZW09aW50ZXJuYWw7bmxhdD01Mi4xMjgzMTExNTcyMjY1NjtubG9uPTIxLjA2MDQ0OTYwMDIxOTcyNw?map=52.1283,21.06013,15,normal" target="_blank">(HERE MAPS)</a> <a href="http://jakdojade.pl/?tc=52.1283:21.060129&tn=Romualda%20Mielczarskiego%201&td=Romualda%20Mielczarskiego%201&cid=3000&aro=1" target="_blank">(dojazd jakdojade.pl)</a-->
+					<p>var <var>o_mnie</var> = {</p>
+					<blockquote>
 					<?php
 						$style=' style="display: none;"';
 
@@ -53,21 +55,23 @@
 							$github=$rekord['git'];
 							$linkedin=$rekord['linkedin'];
 
-							if($identity!="") {$identity='<p'.$style.'><i class="fas fa-id-card"></i> '.$identity.'<p>';}
-							if($www!=""){$www='<p'.$style.'><i class="fa fa-industry"></i> <a href="http://$www" target="_blank">'.$www.'</a></p>';}
-							if($birthday!=""){$birthday='<p'.$style.'><i class="fa fa-birthday-cake"></i> '.$birthday.'</p>';}
-							if($address!=""){$address='<p'.$style.'><i class="fa fa-globe"></i> '.$address.'</p>';}
-							if($phone!=""){$phone='<p'.$style.'><i class="fa fa-phone"></i> <a href="tel:'.substr($phone,0,3).'-'.substr($phone,3,3).'-'.substr($phone,6,2).'-'.substr($phone,8,2).'-'.substr($phone,10,2).'">'.$phone.'</a></p>';}
-							if($e_mail!=""){$e_mail='<p'.$style.'><i class="fa fa-at"></i> <a href="mailto:'.$e_mail.'">'.$e_mail.'</a></p>';}
-							if($skype!=""){$skype='<p'.$style.'> '.$skype.'</p>';}
-							if($facebook!=""){$facebook='<p'.$style.'> '.$facebook.'</p>';}
-							if($github!=""){$github='<p'.$style.'><i class="fab fa-github"></i> <a href="'.$github.'" target="_blank">repozytorium GitHub</a></p>';}
-							if($linkedin!=""){$linkedin='<p'.$style.'><i class="fab fa-linkedin"></i> <a href="'.$linkedin.'" target="_blank">LinkedIn</a></p>';}
+							if($identity!="") {$identity='<p'.$style.'>identity: "'.$identity.'", <i class="fas fa-id-card"></i><p>';}
+							if($www!=""){$www='<p'.$style.'>www: "<a href="http://$www" target="_blank">'.$www.'</a>", <i class="fa fa-industry"></i></p>';}
+							if($birthday!=""){$birthday='<p'.$style.'>birthday: "'.$birthday.'", <i class="fa fa-birthday-cake"></i></p>';}
+							if($address!=""){$address='<p'.$style.'>address: " '.$address.'", <i class="fa fa-globe"></i></p>';}
+							if($phone!=""){$phone='<p'.$style.'>phone: "<a href="tel:'.substr($phone,0,3).'-'.substr($phone,3,3).'-'.substr($phone,6,2).'-'.substr($phone,8,2).'-'.substr($phone,10,2).'">'.substr($phone,0,3).'-'.substr($phone,3,3).'-'.substr($phone,6,2).'-'.substr($phone,8,2).'-'.substr($phone,10,2).'</a>", <i class="fa fa-phone"></i></p>';}
+							if($e_mail!=""){$e_mail='<p'.$style.'>e_mail: "<a href="mailto:'.$e_mail.'">'.$e_mail.'</a>", <i class="fa fa-at"></i></p>';}
+							if($skype!=""){$skype='<p'.$style.'>skype: "'.$skype.'",</p>';}
+							if($facebook!=""){$facebook='<p'.$style.'>facebook: "'.$facebook.'",</p>';}
+							if($github!=""){$github='<p'.$style.'>github: "<a href="'.$github.'" target="_blank">repozytorium GitHub</a>", <i class="fab fa-github"></i></p>';}
+							if($linkedin!=""){$linkedin='<p'.$style.'>linkedin: "<a href="'.$linkedin.'" target="_blank">LinkedIn</a>", <i class="fab fa-linkedin"></i></p>';}
 
 						echo "$identity$www$birthday$address$phone$e_mail$skype$facebook$github$linkedin";
 
 						mysql_close($connection);
 					?>
+					</blockquote>
+					<p>};</p>
 					<br>
 					</div>
 					
